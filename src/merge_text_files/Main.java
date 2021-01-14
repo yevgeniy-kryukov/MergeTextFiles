16b16868691c48dc22b95d009f2a411a8289fdbc
yevgeniy.kryukov <yevgeniy.kryukov.1984@gmail.com>
2021-01-15 03:29:25 +0600

    fixed bags

--- src/merge_text_files/Main.java
+++ src/merge_text_files/Main.java
@@ -8,117 +8,116 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		final String QUIT_TEXT = "quit";
-		String folderPath = "";
-		String outputFileName = "";
+		String folderPath;
+		String outputFileName;
 		
-		Scanner in = new Scanner(System.in);
-
-		do {
-			System.out.print("1. Enter the full path to the directory with files (type \"" + QUIT_TEXT + "\" for exit):");	
-			folderPath = in.nextLine();
-		} while(folderPath.isEmpty());
-				
-		if (folderPath.equalsIgnoreCase(QUIT_TEXT)) return;
-
-		do {
-			System.out.print("2. Enter the name(*.txt(csv)) of the output file (type \"" + QUIT_TEXT + "\" for exit):");	
-			outputFileName = in.nextLine();
-		} while(outputFileName.isEmpty());
-
-		if (outputFileName.equalsIgnoreCase(QUIT_TEXT)) return;
-		
-		in.close();
-		
-		File dir = new File(folderPath);
-		File outputFile = new File(folderPath + "\\" + outputFileName);		
-		
-		if (!dir.exists()) {
-			System.out.println("Directory \"" + dir + "\" not exists");
-			return;
-		}
-		
-		if (!dir.isDirectory()) {
-			System.out.println("\"" + dir + "\" is not directory");
-			return;
-		}
-		
-		ArrayList<File> listFiles = getListFiles(dir, outputFileName);
-		
-		if (listFiles.size() == 0) {
-			System.out.println("Text files not found");
-			return;
-		}
-		
-		if (outputFile.exists()) {
-			System.out.println("Output file is already exists");
-			return;
-		}
-		
-		createOutputFile(outputFile);
-		
-		mergeFiles(listFiles, outputFile);
-	}
-	
-	private static ArrayList<File> getListFiles(File dir, String outputFileName) {
-		ArrayList<File> listFiles = new ArrayList<File>();
-		for (File item: dir.listFiles()) {
-			if ( (!item.getName().equalsIgnoreCase(outputFileName)) && (item.getName().indexOf(".txt") > -1 || item.getName().indexOf(".csv") > -1) ){
-				listFiles.add(item);
-			}
-		}
-		return listFiles;
-	}
-	
-	private static void createOutputFile(File outputFile) {	
 		try {
-			if (!outputFile.createNewFile()) {
-				System.out.println("Output file not created");
+			Scanner in = new Scanner(System.in);
+
+			do {
+				System.out.print("1. Enter the full path to the directory with files (type \"" + QUIT_TEXT + "\" for exit):");	
+				folderPath = in.nextLine();
+			} while(folderPath.isEmpty());
+					
+			if (folderPath.equalsIgnoreCase(QUIT_TEXT)) return;
+
+			do {
+				System.out.print("2. Enter the name(*.txt(csv)) of the output file (type \"" + QUIT_TEXT + "\" for exit):");	
+				outputFileName = in.nextLine();
+			} while(outputFileName.isEmpty());
+
+			if (outputFileName.equalsIgnoreCase(QUIT_TEXT)) return;
+			
+			in.close();
+			
+			File dir = new File(folderPath);
+			File outputFile = new File(folderPath + "\\" + outputFileName);		
+			
+			if (!dir.exists()) {
+				System.out.println("Directory \"" + dir + "\" not exists");
+				return;
 			}
+			
+			if (!dir.isDirectory()) {
+				System.out.println("\"" + dir + "\" is not directory");
+				return;
+			}
+			
+			ArrayList<File> listFiles = getListFiles(dir, outputFileName);
+			
+			if (listFiles.size() == 0) {
+				System.out.println("Text files not found");
+				return;
+			}
+			
+			if (outputFile.exists()) {
+				System.out.println("Output file is already exists");
+				return;
+			}
+			
+			createOutputFile(outputFile);
+			
+			mergeFiles(listFiles, outputFile);
+		} catch (UnsupportedEncodingException ex) {
+			System.out.println("UnsupportedEncodingException: " + ex.getMessage());
 		} catch (IOException ex) {
-			System.out.println(ex.getMessage());
+			System.out.println("IOException: " + ex.getMessage());
+		} catch (Exception ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
+	}
+
+	private static ArrayList<File> getListFiles(File dir, String outputFileName) throws IOException {
+		File[] sourceListFiles = dir.listFiles();
+		ArrayList<File> targetListFiles = new ArrayList<>();
+		for (File item: sourceListFiles) {
+			if ( (!item.getName().equalsIgnoreCase(outputFileName)) && (item.getName().contains(".txt") || item.getName().contains(".csv")) ){
+				targetListFiles.add(item);
+			}
+		}
+		return targetListFiles;
+	}
+	
+	private static void createOutputFile(File outputFile) throws IOException {
+		if (!outputFile.createNewFile()) {
+			System.out.println("Output file not created");
 		}
 	}
 	
-	private static void mergeFiles(ArrayList<File> listFiles, File outputFile) {
+	private static void mergeFiles(ArrayList<File> listFiles, File outputFile) throws IOException {
 		String str;
+
+		FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
 		
-		try {
+		for (File item: listFiles) {
+			FileInputStream fileInputStream = new FileInputStream(item);
+			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 			
-			FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
-			BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-			
-			for (File item: listFiles) {
-				FileInputStream fileInputStream = new FileInputStream(item);
-				InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
-				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-				
-				while ((str = bufferedReader.readLine()) != null) {
-					bufferedWriter.write(str);
-					bufferedWriter.newLine();
-				}
-				
-				fileInputStream.close();
-				inputStreamReader.close();
-				bufferedReader.close();
+			while ((str = bufferedReader.readLine()) != null) {
+				bufferedWriter.write(str);
+				bufferedWriter.newLine();
 			}
 			
-			bufferedWriter.flush();
-			
-			fileOutputStream.close();
-			outputStreamWriter.close();
-			bufferedWriter.close();
-			
-		} catch (IOException ex) {
-			System.out.println(ex.getMessage());
+			fileInputStream.close();
+			inputStreamReader.close();
+			bufferedReader.close();
 		}
+		
+		bufferedWriter.flush();
+		
+		fileOutputStream.close();
+		outputStreamWriter.close();
+		bufferedWriter.close();
 	}
 }
